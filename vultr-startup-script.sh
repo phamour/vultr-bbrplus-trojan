590e6364e13369f0d55e5fa681ab4242268ca687
yipengh <huang.ypeng@gmail.com>
2020-02-16 18:13:07 +0100

    Download script before executing bbrplus installation

--- vultr-startup-script.sh
+++ vultr-startup-script.sh
@@ -6,10 +6,10 @@ apt-get update -y & apt-get install -y curl xz-utils
 
 cd ~
 
+wget -N --no-check-certificate "https://raw.githubusercontent.com/yipengh/vultr-bbrplus-trojan/master/install.sh"
+chmod +x install.sh
+
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
 chmod +x tcp.sh
 
 printf '2\ny\n' | DEBIAN_FRONTEND=noninteractive ./tcp.sh
-
-wget -N --no-check-certificate "https://raw.githubusercontent.com/yipengh/vultr-bbrplus-trojan/master/install.sh"
-chmod +x install.sh
