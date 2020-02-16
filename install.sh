c4b0df1f376314d03629d864c40f5e71bf6bfc2a
yipengh <huang.ypeng@gmail.com>
2020-02-16 18:23:59 +0100

    Download Trojan.sh instead of remote execution

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@
 printf '7\n' | ./tcp.sh
 
 # Install Trojan
-printf '1\n' | bash <(curl -s -L https://github.com/V2RaySSR/Trojan/raw/master/Trojan.sh)
+printf '1\n' | ./Trojan.sh
--- vultr-startup-script.sh
+++ vultr-startup-script.sh
@@ -9,6 +9,9 @@ cd ~
 wget -N --no-check-certificate "https://raw.githubusercontent.com/yipengh/vultr-bbrplus-trojan/master/install.sh"
 chmod +x install.sh
 
+wget -N --no-check-certificate "https://github.com/V2RaySSR/Trojan/raw/master/Trojan.sh"
+chmod +x Trojan.sh
+
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
 chmod +x tcp.sh
 
